075a5a9df0d2ef055522351df78583b140e97d45
Doriane Papilloud <doriane.papillou@hes-so.ch>
2022-05-03 15:15:36 +0200

    Advancement random

--- DoBeFly/DoBeFlyContext.cs
+++ DoBeFly/DoBeFlyContext.cs
@@ -9,15 +9,17 @@ namespace DoBeFly
 {
     public class DoBeFlyContext : DbContext
     {
-        public DbSet<Flight> Flightset { get; set; }
+        public DbSet<Flight> FlightSet { get; set; }
         public DbSet<Pilot> PilotSet { get; set; }
         public DbSet<Passenger> PassengerSet { get; set; }
         public DbSet<Booking> BookingSet { get; set; }
 
         //SQL Express
 
-        public static string ConnexionString { get; set; } = @"Server=(localDB)\MSSQLLocalDB;Database=DoBeFly;"+
-                                                            "Trusted_Connection=True;App=EFCoreApp2021;MultipleActiveResultSets=true";
+        //public static string ConnexionString { get; set; } = @"Server=(localDB)\MSSQLLocalDB;Database=DoBeFly;"+
+                                                            //"Trusted_Connection=True;App=EFCoreApp2021;MultipleActiveResultSets=true";
+
+        public static string ConnexionString { get; set; } = "Data Source=153.109.124.35;Initial Catalog=DoBeFly;User ID=6231db;Password=Pwd46231.;Pooling=False";
 
 
         public DoBeFlyContext() { }
--- DoBeFly/Program.cs
+++ DoBeFly/Program.cs
@@ -22,9 +22,48 @@ namespace DoBeFly
 
             //ctx.EmployeeSet.Add(employee);
 
-            Console.WriteLine();
+            Console.WriteLine("Employee added");
+
+
+
+            //add an employee
+            Pilot pilotOne = new Pilot { Surname = "Jean-Claude Dusse", Salary = 10000 };
+
+            ctx.PilotSet.Add(pilotOne);
+
+            Console.WriteLine("Pilot added");
 
             ctx.SaveChanges();
+
+
+            foreach (Flight f in ctx.FlightSet)
+                Console.WriteLine("Date: {0}, Destination: {1}, Seats: {2}", f.Date, f.Destination, f.Seats);
+
+            var FlightsToBoraBora = ctx.FlightSet.Where(f => f.Destination == "BoraBora" && f.Seats < 100).ToList<Flight>();
+
+            var FlightToBoraBoraTwo = from Flight in ctx.FlightSet
+                                      where Flight.Destination == "BoraBora" && Flight.Seats < 100
+                                      select Flight;
+
+            //this is to display
+            var FlightToBoraBoraThree = FlightToBoraBoraTwo.ToList();
+
+            foreach (Flight f in ctx.FlightsToBoraBoraTwo)
+                Console.WriteLine("Date: {0}, Destination: {1}, Seats: {2}", f.Date, f.Destination, f.Seats);
+
+            Booking b1 = new Booking() { flight = f2, Passenger = new Passenger() { Surname = "Gates", GivenName = "Bill" } };
+
+            Passenger p1 = new Passenger() { Surname = "Musk", GivenName = "Elon" };
+
+            ctx.PassengerSet.Add(p1);
+            ctx.BookingSet.Add(b1);
+
+
+            Booking b2 = new Booking() { flight = f4, Passenger = p1 };
+            ctx.BookingSet.Add(b2);
+
+            ctx.SaveChanges();
+
 
         }
 
