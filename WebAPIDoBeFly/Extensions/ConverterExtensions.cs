e92338b9f801c85d3f3463721e32ec76bc1770bc
Doriane Papilloud <doriane.papillou@hes-so.ch>
2022-05-17 13:49:53 +0200

    Advancement flightM

---
+++ WebAPIDoBeFly/Extensions/ConverterExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIDoBeFly.Extensions
+{
+    public static class ConverterExtensions
+    {
+
+        public static Models.FlightM ConvertToFlightM (this DoBeFly.Flight f)
+        {
+
+            Models.FlightM fM = new Models.FlightM();
+            fM.Date = f.Date;
+            fM.Departure = f.Departure;
+            fM.Destination = f.Destination;
+            fM.FlightNo = f.FlightNo;
+
+            return fM;
+
+        }
+
+        public static DoBeFly.Flight ConvertToFlightDoBeFly (this Models.FlightM fM)
+        {
+
+            DoBeFly.Flight f = new DoBeFly.Flight();
+            f.Date = fM.Date;
+            f.Departure = fM.Departure;
+            f.Destination = fM.Destination;
+            f.FlightNo = fM.FlightNo;
+
+            return f;
+
+        }
+
+    }
+}
---
+++ WebAPIDoBeFly/Models/FlightM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIDoBeFly.Models
+{
+    public class FlightM
+    {
+
+        public int FlightNo { get; set; }
+        public DateTime Departure { get; set; }
+        public string Destination { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
